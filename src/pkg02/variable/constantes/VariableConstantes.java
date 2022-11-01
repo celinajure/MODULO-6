04f9775b4c1fbf25255f1b343c4a8c6256815024
celinajure <celinajure@localhost>
2022-11-01 20:02:55 -0300

    conexión a github

---
+++ src/pkg02/variable/constantes/VariableConstantes.java
@@ -0,0 +1,24 @@
+
+package pkg02.variable.constantes;
+
+
+public class VariableConstantes {
+
+    
+    public static void main(String[] args) {
+        int numero;
+        String nombre;
+        char letra;
+                
+                
+       numero= 5;
+       nombre = "Celina";
+       letra = 'C';
+       double temperatura = 22.5;
+       boolean siONo = false;
+       long numeroLargo= 123456790;
+       // ahora quiero mostrar por pantalla una CONCATENACION
+        System.out.println("veamos: " + numero + ", veamos: " + nombre + ", veamos: " + letra);
+    }
+    
+}
